c86bb76dc8b8e0c85720909b11794c84e22118f2
Faturrahman <faturrahmanmakruf@gmail.com>
2019-06-10 13:46:09 +0700

    not 7 ice servers

--- gettoken.php
+++ gettoken.php
@@ -2,7 +2,7 @@
 
 $curl = curl_init();
 curl_setopt_array( $curl, array (
-    CURLOPT_URL => "https://global.xirsys.net/_token/sample?k=".$_POST["username"],
+    CURLOPT_URL => "https://global.xirsys.net/_token/sample2?k=".$_POST["username"],
     CURLOPT_USERPWD => "akupintar:f0f07904-3e31-11e9-820d-0242ac110003",
     CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
     CURLOPT_CUSTOMREQUEST => "PUT",
